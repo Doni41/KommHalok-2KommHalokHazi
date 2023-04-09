ef25bf42ee852351cbebc0a7b146ab913fd4dd48
Doni41 <55320466+Doni41@users.noreply.github.com>
2023-04-09 19:18:51 +0200

    rendesen lefutott script

--- client.py
+++ client.py
@@ -17,22 +17,22 @@ class BinaryFileOpenerAndReader:
         self.printUnpackedData(self.readFourth())
 
         # 2 feladat
-        self.printPackedData(struct.Struct('f ? c'), self.readFirst())
-        self.printPackedData(struct.Struct('c 9s i'), self.readSecond())
-        self.printPackedData(struct.Struct('i ? f'), self.readThird())
-        self.printPackedData(struct.Struct('c f 9s'), self.readFourth())
+        self.printPackedData(struct.Struct('15s i ?'), ('elso'.encode(), 83, True))
+        self.printPackedData(struct.Struct('f ? c'), (86.5, False, 'X'.encode()))
+        self.printPackedData(struct.Struct('i 13s f'), (74, 'masodik'.encode(), 93.9))
+        self.printPackedData(struct.Struct('c i 16s'), ('Z'.encode(), 105, 'harmadik'.encode()))
 
     def readFirst(self):
         with open(self.files[1], "rb") as f:
             self.first_file = f.read(6)
-            packer = struct.Struct('f ? c')
+            packer = struct.Struct('f ? 1s')
             unpacked_data = packer.unpack(self.first_file)
         return unpacked_data
     
     def readSecond(self):
         with open(self.files[2], "rb") as f:
             self.second_file = f.read(16)
-            packer = struct.Struct('c 9s i')
+            packer = struct.Struct('1s 9s i')
             unpacked_data = packer.unpack(self.second_file)
             return unpacked_data
     
@@ -46,7 +46,7 @@ class BinaryFileOpenerAndReader:
     def readFourth(self):
         with open(self.files[4], "rb") as f:
             self.fourth_file = f.read(17)
-            packer = struct.Struct('c f 9s')
+            packer = struct.Struct('1s f 9s')
             unpacked_data = packer.unpack(self.fourth_file)
             return unpacked_data
     
@@ -55,9 +55,8 @@ class BinaryFileOpenerAndReader:
         print(data)
 
     # az unpack-elt bináris tartalmat printeli ki
-    def printPackedData(self, packer, packed_data):
-        print(packer.pack(*packed_data))
-
+    def printPackedData(self, packer, values):
+        print(packer.pack(*values))
 
 binaryFileOpenerAndReader = BinaryFileOpenerAndReader(sys.argv)
 binaryFileOpenerAndReader.printDatas()
