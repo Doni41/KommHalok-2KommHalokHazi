4c7f96853146f893dc1fee5c6ff69e8089470c48
Doni41 <55320466+Doni41@users.noreply.github.com>
2023-04-09 17:27:58 +0200

    teszteles javitasa

--- test.py
+++ test.py
@@ -15,7 +15,61 @@ def readFromBinaryFile():
             packed_data = packer.unpack(file_content)
             print(packed_data)
             print(packer.pack(*packed_data))
+
+
+def writeToBinaryFile2():
+    with open('dates2.bin', 'wb') as f:
+        values = ('a'.encode(), '123456789'.encode(), 235)
+        packer = struct.Struct('c 9s i')
+        packer_data = packer.pack(*values)
+        f.write(packer_data)
+
+
+def readFromBinaryFile2():
+      with open('dates2.bin', 'rb') as f:
+            file_content = f.read(16)
+            print(file_content)
+            packer = struct.Struct('c 9s i')
+            packed_data = packer.unpack(file_content)
+            print(packed_data)
+            print(packer.pack(*packed_data))
+
+def writeToBinaryFile3():
+    with open('dates3.bin', 'wb') as f:
+        values = (124, False, 44.56)
+        packer = struct.Struct('i ? f')
+        packer_data = packer.pack(*values)
+        f.write(packer_data)
+
+
+def readFromBinaryFile3():
+      with open('dates3.bin', 'rb') as f:
+            file_content = f.read(12)
+            print(file_content)
+            packer = struct.Struct('i ? f')
+            packed_data = packer.unpack(file_content)
+            print(packed_data)
+            print(packer.pack(*packed_data))
+
+
+def writeToBinaryFile4():
+    with open('dates4.bin', 'wb') as f:
+        values = ('f'.encode(), 55.123, '987654321'.encode())
+        packer = struct.Struct('c f 9s')
+        packer_data = packer.pack(*values)
+        f.write(packer_data)
+
+
+def readFromBinaryFile4():
+      with open('dates4.bin', 'rb') as f:
+            file_content = f.read(17)
+            print(file_content)
+            packer = struct.Struct('c f 9s')
+            packed_data = packer.unpack(file_content)
+            print(packed_data)
+            print(packer.pack(*packed_data))
 	
-readFromBinaryFile()
+# writeToBinaryFile4()
+readFromBinaryFile4()
 
 
